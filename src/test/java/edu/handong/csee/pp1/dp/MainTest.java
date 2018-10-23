d5628f8fab7dff92a4db8307220bdb287747d287
dpcks <dpcks@LEEYECHAN>
2018-10-23 21:44:47 +0900

    Finished TASK 2

--- src/test/java/edu/handong/csee/pp1/dp/MainTest.java
+++ src/test/java/edu/handong/csee/pp1/dp/MainTest.java
@@ -26,5 +26,24 @@ public class MainTest {
 		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
 		// 생성이 된 후 commit/push해야 함.
 		// 위 test main을 참고하여 실행하면 됨.
+		String[] args = {"D:\\1Yechan\\18-2\\실전프로젝트01\\Big_data\\java","data/java.arff"};
+		// String[] args = {"/use/this/style/for/your/path"}; // for mac users
+		
+		assertEquals(args.length,2);
+		
+		Main.main(args);
+	}
+	
+	@Test
+	public void testForTasks3() {
+		// TASK2 TODO GitHub에서 작은 규모의 자바 프로젝트를 2개를 찾아, arff파일을 생성하시오. 파일이름은 [project이름].arff data디렉터리에 저장되게 하고
+		// 생성이 된 후 commit/push해야 함.
+		// 위 test main을 참고하여 실행하면 됨.
+		String[] args = {"D:\\1Yechan\\18-2\\실전프로젝트01\\Big_data\\java-1","data/java-1.arff"};
+		// String[] args = {"/use/this/style/for/your/path"}; // for mac users
+		
+		assertEquals(args.length,2);
+		
+		Main.main(args);
 	}
 }
